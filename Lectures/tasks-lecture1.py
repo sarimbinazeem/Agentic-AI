881bab05625685c3c4c2d28d785e0b12415d024c
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-07-03 03:52:54 -0700

    Task lecture file updated

--- Lectures/tasks-lecture1.py
+++ Lectures/tasks-lecture1.py
@@ -225,7 +225,53 @@ def task5():
     persona = "You are a chef who only talks about food and cooking."  # change this!
     history = [{"role": "system", "content": persona}]
     print(f"Persona: {persona}\nType 'quit' to exit.\n")
-    # YOUR CODE HERE
+    
+    while True:
+        prompt = input("What do you want to ask today: ")
+
+        #if the user enters quit we break the loop 
+        if prompt.lower().strip() == "quit":
+            break
+            
+        #if the user accidently enters a blank space we ignore and continue
+        if prompt.strip() == "":
+            continue
+            
+        history.append(
+                    {
+                        "role":"user",
+                        "content":prompt
+                    })
+            
+        response = client.chat.completions.create(
+            model = os.getenv("MODEL"),
+                
+            messages=history,
+            stream = True,
+                
+        )
+        #since the response is on chunks so we apply loop to obtain response in a variable
+        assistant_message = ""
+    
+        for chunk in response:
+            if not chunk.choices:
+                continue
+            
+            delta = chunk.choices[0].delta.content or ""
+            print(delta,end="",flush=True)
+            
+            assistant_message += delta
+            
+        #to get in next line
+        print("\n")
+            
+            
+        history.append(
+            {
+                "role":"assistant",
+                "content": assistant_message,
+            }
+        )
 
 
 

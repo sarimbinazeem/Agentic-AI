950862f71a4fc792885ea446e89afa378f8a6336
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-07-03 04:31:12 -0700

    added task 6 in tasks file

--- Lectures/tasks-lecture1.py
+++ Lectures/tasks-lecture1.py
@@ -287,7 +287,40 @@ def task5():
 
 def task6():
     print("=== TASK 6: Language Translator ===")
-    # YOUR CODE HERE
+    language = input("To which language you want to translate to? ")
+    sentence = input("Enter A Sentence You Want To Translate: ")
+
+    history = [
+        {
+            "role":"system",
+            "content": f"You are a language translator that translates an english sentence to {language}. Return only the translation please"
+            
+        },
+        
+        {
+            "role":"user",
+            "content":sentence,
+        }
+    ]
+
+    response = client.chat.completions.create(
+        model=os.getenv("MODEL"),
+        messages=history,
+        
+        stream=True,
+    )
+
+    for chunk in response:
+        if not chunk.choices:
+            continue
+        
+        delta=chunk.choices[0].delta.content or ""
+        
+        print(delta,end="",flush=True)
+        
+    
+    
+    
 
 
 

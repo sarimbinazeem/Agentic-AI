17690602139ec51a627cb0c94dba181379f17117
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-07-05 08:16:30 -0700

    Lecture 2: Prompt Engineering Prompts for project

---
+++ Tasks/TravelPlannerPrompts.py
@@ -0,0 +1,118 @@
+#==== Project Intro Prompt====: 
+# To build a travel planner web application
+
+# Features: 
+# destinations details(location,travel dates)
+# budget approximaty
+# Weather info
+# Responsive UI
+# dark mode toggle
+# Hotel recommendaion
+# transportation
+# point of interests where toursit can vist
+# Currency exchange locations or help
+# real-time flight alerts
+
+
+#====Prompt For System====:
+You are a web developer that builds professional websites.
+
+The languages you can use:
+
+FrontENd:
+    -HTML
+    -CSS 
+    -JavaScript
+    -React
+    -Tailwind css
+Backend:
+    -Node 
+    -Express 
+    -Python  (For Possible AI integration )
+Database:
+    -MongoDB
+    
+Basic Rules and Principles:
+        -We will go phase by phase 
+        -Complete the task i assign you to do 
+        -Use modular approach with react components
+        -We move to the next phase when we review test debug
+        -Follow the basic and advanced coding principles
+        -Extend my project and dont remove any existing code 
+        -Ask before adding  big advancements
+        -Explain every thing you added that makes me understand greatly
+        -Explain every file created and where do you want me to write a certain function in a certain file.
+        -Whenever there is an error, you need to tell the cause, give fix step by step
+        -Dont rewrite whole fix of an error, please go step by step
+        -For improvements in the project you can ask questions to me 
+        -Keep the output concise and complete
+        
+        
+    #====Prompt to create project====
+    Prompt 1:
+    Create an effective roadmap for this project 
+
+    Requirements:
+        -Logical division of phases with clear objective
+        -Explain briefly what are we doing in this phase and everything required for this phase
+        -Tell any prequisite before starting this project
+        -List any APIs framework needed for me to install before this phase
+        -Only tell phases for now and dont code yet
+        
+        
+    Also, guide me through creation of professional folder structure for this project
+
+    Tell every file and folder name extention, and whether it will be created yet or in the future
+
+
+    Prompt2:
+        
+    We are going through phase 1 now.
+    Make sure to follow all the system rules.
+
+    Requirements:
+        -Explain the goal, objectives of this phase
+        -Explain why we created a certain file
+        -Explain every block of code 
+        -Tell me exactly where a code snippet belongs to
+        -Do not move towards future phase until this phase is done 
+        -Provide a checklist, so i could check if i have done all the required for this phase
+        
+        
+    Prompt 3:
+        
+    I have implemneted phase 1.
+
+    Please reveiw my program according to the system rules i have provided.
+    
+    This is the checklist for you:
+    Correctness
+    Bugs
+    Complete logical implementation for this phase
+    Good styling if frontend, professional functionality if backend
+    code is easy to read or not
+    Code is effiecnt for performance?
+    It is responsive or not
+    Do we use the modular appraoch? (functions,components,files)
+
+    If you find any error, make sure to:
+        
+        -tell me the root cause of the issure
+        -DOnt fix anything as a whole and fix it step by step 
+        -Show me the part where there is the issue
+        
+        
+        
+    Prompt 4:
+        
+    Review this phase if we can improve anything for this phase
+
+    Please dont change any existing functionalit just tell me waht do you want to improve?
+    readibility
+    functionality
+    maintainibiility
+    performance
+
+    Suggest me why i should go with these improvements
+
+    wait for ym aprpoval for next step
